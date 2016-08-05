929f79471a061e5a554045fd6fe633db24993992
Johan Kotlinski <kotlinski@gmail.com>
2016-08-05 22:06:17 +0200

    define W2

--- runtime.cc
+++ runtime.cc
@@ -14,6 +14,7 @@ MSB = $73 ; msb stack is [$3b, $72]
 LSB = $3b ; lsb stack is [3, $3a]
 
 W = $8b ; rnd seed
+W2 = $8d ; rnd seed
 
 * = $801
 !byte $b, $08, $a, 0, $9E, $32, $30, $36, $31, 0, 0, 0 ; basic header
